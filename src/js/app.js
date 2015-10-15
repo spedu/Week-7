0cec9928d2b7d33488f98d867fb8917690ff77b3
Scott Pullen <scottpullen@g.harvard.edu>
2015-10-14 20:49:57 -0400

    removing old code

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,1 @@
-/*
-$(document).ready(function(){
-  var fizzbuzz = 0;
-  $('#plus-btn').click(function(){
-    fizzbuzz++;
-    $('#fizzbuzz').html(fizzbuzz);
-  });
-  $('#fizzbuzz').html(fizzbuzz);
-});
-*/
-
 angular.module('fizzbuzzer', []).controller('MyController', function(){});
